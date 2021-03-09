0c615ec9c983812e0e0dc33e3653d6a3e6e072b0
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-03-09 11:51:53 -0500

    server.js view and layout

---
+++ controllers/burgController.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+const burger = require('../models/burg.js');
+
+// Create all our routes and set up logic within those routes where required.
+router.get('/', (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgs: data,
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('burgs', (req, res) => {
+  burger.create(['/burger_name', 'noshed'], [req.body.burger_name, req.body.noshed], (result) => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put('/burgs/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      noshed: req.body.noshed,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+router.delete('/burgs/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  burger.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burg.js
@@ -0,0 +1,18 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require('../config/orm.js');
+
+const burger = {
+  all(cb) {
+    orm.all('burgs', (res) => cb(res));
+  },
+  // The variables cols and vals are arrays.
+  create(cols, vals, cb) {
+    orm.create('burgs', cols, vals, (res) => cb(res));
+  },
+  delete(condition, cb) {
+    orm.delete('burgs', condition, (res) => cb(res));
+  },
+};
+
+// Export the database functions for the controller (burgsController.js).
+module.exports = burger;
